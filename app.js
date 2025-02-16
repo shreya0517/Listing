b6027c143031ad6bca4c6ca86227492631fc0553
shreya0517 <shreyagarg1656@gmail.com>
2025-02-16 20:08:38 +0530

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express= require ("express");
 const app = express();
 const mongoose = require ("mongoose");
 const Listing = require("./models/listing.js");
+const path= require("path");
 
 const mongo_URL="mongodb://127.0.0.1:27017/wonderlst"
 
@@ -19,26 +20,35 @@ async function main() {
     await mongoose.connect(mongo_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 //api
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
-//accessing listing
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title:"My New Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India",
-    });
+app.get("/listings", async (req, res) => {
 
-    //saving sample listing and always use await with async function
-    await sampleListing.save();
-    console.log("Sample was saved");
-    res.send("Successful testing");
+    const allListings= await Listing.find({});
+    res.render("/listings/index.ejs", {allListings});
 });
+
+// accessing listing
+//  app.get("/testListing", async (req, res) => {
+//    let sampleListing = new Listing({
+//          title:"My New Villa",
+//          description: "By the beach",
+//          price: 1200,
+//          location: "Calangute, Goa",
+//          country: "India",
+//      });
+
+//     //saving sample listing and always use await with async function
+//     await sampleListing.save();
+//     console.log("Sample was saved");
+//     res.send("Successful testing");
+// });
 
 app.listen(8080, () => {
     console.log("server is listening on port 8080");
